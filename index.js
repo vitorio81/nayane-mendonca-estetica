3f28f66c7575170fecd6a438cb9ea5353b9e79d7
Vitório Mota <154383083+vitorio81@users.noreply.github.com>
2024-04-25 22:06:24 -0300

    Terminando algumas modificações

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 function exibirMenu() {
-    const ulOptions = document.getElementById('ul-options');
-    if(ulOptions.style.display === 'block'){
-        ulOptions.style.display = 'none'
-    } else{
-        ulOptions.style.display = 'block'
+
+    if (ulOptions === 'block') {
+        ulOptions.style.display = 'none';
+    } else {
+        ulOptions.style.display = 'block';
     }
 }
 
-function novoTamanho(){
-    const ulOptions = document.getElementById('ul-options')
-    if (window.innerWidth >= 576) {
-        const ulOptions = document.getElementById('ul-options').style.display = 'block'
-    } else {
-        const ulOptions = document.getElementById('ul-options').style.display = 'none'
-    }
-}
