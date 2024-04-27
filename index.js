c59f8e7d83db119a0c35dd48303cda747b2a8084
Vitório Mota <154383083+vitorio81@users.noreply.github.com>
2024-04-26 22:49:35 -0300

    finalizando web site

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function exibirMenu() {
 
-    if (ulOptions === 'block') {
-        ulOptions.style.display = 'none';
+    if (itens.display == 'block') {
+        itens.style.display = 'none';
     } else {
-        ulOptions.style.display = 'block';
+        itens.style.display = 'block';
     }
 }
 
+function esconderMenu(){
+    itens.style.display = 'none';
+}
+
+const myObserver = new IntersectionObserver ((observer) => {
+    observer.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.animacao')
+
+elements.forEach((element) => myObserver.observe(element))
